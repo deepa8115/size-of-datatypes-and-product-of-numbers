5e99ba62813345b6c7ce4822567c0334c2d8f7e3
deepa8115 <65079662+deepa8115@users.noreply.github.com>
2020-06-22 18:00:36 +0530

    Create Sizeandproduct.cpp
    This file contains size of datatypes and product of numbers programs.

---
+++ Sizeandproduct.cpp
@@ -0,0 +1,24 @@
+//program no.1 size of datatypes//
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"size of int :"<<sizeof(int)<<"bytes"<<endl;
+cout<<"size of float :"<<sizeof(float)<<"bytes"<<endl;
+cout<<"size of double :"<<sizeof(double)<<"bytes"<<endl;
+cout<<"size of char :"<<sizeof(char)<<"byte"<<endl;
+return 0;
+}
+
+//program no.2 product of numbers//
+#include<iostream>
+using namespace std;
+int main()
+{
+float a, b, product;
+cout<<"enter two numbers:"<<endl;
+cin>>a>>b;
+product=a*b;
+cout<<"product="<<product<<endl;
+return 0;
+}
